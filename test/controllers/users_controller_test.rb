490acc2e84ce5223fd88b8c053c9329d8caebb2b
EzraWeller <ezra.weller@gmail.com>
2017-07-01 16:21:12 -0400

    Finish user edit, update, index, and destroy actions

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -25,6 +25,13 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_url
   end
 
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: { user: { admin: true } }
+    assert_not @other_user.reload.admin?
+  end
+
   test "should redirect edit when logged in as wrong user" do
     log_in_as(@other_user)
     get edit_user_path(@user)
@@ -44,11 +51,19 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     get users_path
     assert_redirected_to login_url
   end
-  
-  test "should not allow the admin attribute to be edited via the web" do
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
     log_in_as(@other_user)
-    assert_not @other_user.admin?
-    patch user_path(@other_user), params: { user: { admin: true } }
-    assert_not @other_user.reload.admin?
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
   end
 end
